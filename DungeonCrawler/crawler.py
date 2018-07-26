0d30d69274e21526f6e79d8c6ac5dfd47b742909
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-26 16:00:41 -0600

    Slight formatting fixes
    Formatting fixes and added more text to the def for  move feedback

--- DungeonCrawler/crawler.py
+++ DungeonCrawler/crawler.py
@@ -32,16 +32,13 @@ def updatePosition(dirMoved):
                 print(moveFeedback('South'))
             elif player_instance.direction == 1 and player_instance.pos[1] >= 1: # pointing west
                 player_instance.pos[1] -= 1
-                print("You're now heading West", '\n')
-                print('As you clumsily move ' + direction + ' you take in your new surroundings.')
+                print(moveFeedback('West'))
             elif player_instance.direction == 2 and player_instance.pos[0] >= 1: # pointing north
                 player_instance.pos[0] -= 1
-                print("You're now heading North", '\n')
-                print('As you clumsily move ' + direction + ' you take in your new surroundings.')
+                print(moveFeedback('North'))
             elif player_instance.direction == 3 and player_instance.pos[1] <= 7: # pointing east
                 player_instance.pos[1] += 1
-                print("You're now heading East", '\n')
-                print('As you clumsily move ' + direction + ' you take in your new surroundings.')
+                print(moveFeedback('East'))
             else :
                 print("As you try and move forward you smack right into a wall.", '\n')
 
@@ -58,16 +55,19 @@ def updatePosition(dirMoved):
                 player_instance.direction = 0
         print( player_instance.pos, " ", player_instance.direction)
 
-        # her we check if the players position is the same as an item and if so we tell them and let them choose to interact with the item
+        # her we check if the players position is the same as an item and if so we tell them
+        # and let them choose to interact with the item
+
         locatedItem = findItemInList(player_instance.pos, itemLocations)
         if locatedItem != '':
             print('we found a {0} '.format(locatedItem), player_instance.pos)
         else:
             updatePosition(waitingForMove())
     except ValueError as e:
-        print('Hit an error: ' + e)
+        print('Hit an error: ' + str(e))
 
-def findItemInList(pair, aList):# checks for coordinate pair in list
+
+def findItemInList(pair, aList):  # checks for coordinate pair in list
     for i in range(1, len(aList)):
         if aList[i] == pair:
             return itemDefs[i][0]
@@ -84,8 +84,11 @@ class player:
     print('player created', '\n')
 
 # key location is now randomized per session
+
+
 itemLocations = [[0,4],[r.randrange(0,8),r.randrange(0,8)],[4,4],[6,6]]
 itemDefs = [['exit', 1], ['key', 3], ['pillar', 2], ['pillar', 4]]
+
 
 def buildRoom(x, y, itemList):
     xAxis = x
@@ -104,11 +107,14 @@ def buildRoom(x, y, itemList):
 
     np.asarray(room).reshape(8,8)
     return room
+
+
 def waitingForMove():
     keyPress = ''
     while keyPress == '':
         keyPress = input('Choose a direction (ahead, back, left right): ')
     return keyPress
+
 
 player_instance = player()
 print(buildRoom(8, 8, itemLocations))
