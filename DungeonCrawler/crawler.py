63c6b36dcfc652dea51d727755bdc7a6d253dc56
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-26 17:24:27 -0600

    movement input adjusted
    Taking the first letter to lowercase and passing along we get the same result plus this allows for mispelling or simply typing the first letter for speed sake.

--- DungeonCrawler/crawler.py
+++ DungeonCrawler/crawler.py
@@ -3,9 +3,11 @@ import time as t
 import random as r
 from playerClass import player
 
-# things to add: except statements, break classes out into separate objects
+# things to add: except statements... more except statements
 # decorators? @ ... do high score.. save out to file and read in from file (high score is time based)
 # add pillars vs rocks into dungeon which acts as a wall, cannot walk into that space
+# key when found needs to be stored in a list/etc to be used when we return to the door.
+# not adding the need to actually pick up the key, this will be automatic
 
 
 def intro():
@@ -21,18 +23,19 @@ def intro():
 def updatePosition(dirMoved):
     direction = dirMoved
 
-    def moveFeedback(cardinal, dir = 'ahead'):
+    def moveFeedback(cardinal, dir = 'a'):
 
-        if dir is 'ahead':
-            feedback = "You're now heading " + cardinal + ". As you clumsily move " + str(direction) + " you take in your new surroundings.\nAll you can see surrounding you is darkness.\n"
-        elif dir is 'back':
+        if dir is 'a':
+            feedback = "You're now heading " + cardinal + ". As you clumsily move ahead." +\
+                       " you take in your new surroundings.\nAll you can see surrounding you is darkness.\n"
+        elif dir is 'b':
             feedback = "You take a step back.\nYou're still facing " + cardinal + '\n'
         else:
             feedback = "You're now facing " + cardinal + '\n'
         return feedback
     try:
 
-        if direction == 'ahead':
+        if direction == 'a':
             if player_instance.direction == 0 and player_instance.pos[0] <= 7: # pointing south
                 player_instance.pos[0] += 1
                 print(moveFeedback('South'))
@@ -48,20 +51,20 @@ def updatePosition(dirMoved):
             else :
                 print("As you try and move forward you smack right into a wall.", '\n')
 
-        elif direction == 'back':
+        elif direction == 'b':
             player_instance.pos[0] -= 1
-            print(moveFeedback(str(directionList[player_instance.direction]), 'back'))
+            print(moveFeedback(str(directionList[player_instance.direction]), 'b'))
 
-        elif direction == 'left':
+        elif direction == 'l':
             player_instance.direction -= 1
             if player_instance.direction <= -1:
                 player_instance.direction = 3
-            print(moveFeedback(str(directionList[player_instance.direction]), 'left'))
-        elif direction == 'right':
+            print(moveFeedback(str(directionList[player_instance.direction]), 'l'))
+        elif direction == 'r':
             player_instance.direction +=1
             if player_instance.direction >= 4:
                 player_instance.direction = 0
-            print(moveFeedback(str(directionList[player_instance.direction]), 'right'))
+            print(moveFeedback(str(directionList[player_instance.direction]), 'r'))
         print( player_instance.pos, " ", player_instance.direction)
 
         # her we check if the players position is the same as an item and if so we tell them
@@ -88,13 +91,10 @@ def findItemInList(pair, aList):  # checks for coordinate pair in list
         else:
             return ''
 
-
-
-
 # key location is now randomized per session
 
 
-itemLocations = [[0,4],[r.randrange(0,8),r.randrange(0,8)],[4,4],[6,6]]
+itemLocations = [[0, 4], [r.randrange(0, 8), r.randrange(0, 8)], [4, 4], [6, 6]]
 itemDefs = [['exit', 1], ['key', 3], ['pillar', 2], ['pillar', 4]]
 directionList = ['South', 'West', 'North', 'East']
 
@@ -103,7 +103,7 @@ def buildRoom(x, y, itemList):
     xAxis = x
     yAxis = y
     itemCount = -1
-    room = np.zeros([y,x])
+    room = np.zeros([y, x])
 
     for i in itemList:
         if itemCount == -1:
@@ -118,11 +118,11 @@ def buildRoom(x, y, itemList):
     return room
 
 
-def waitingForMove():
+def waitingForMove():  # takes first letter of the word typed to lowercase and passes it along.
     keyPress = ''
     while keyPress == '':
-        keyPress = input('Choose a direction (ahead, back, left right): ')
-    return keyPress
+        keyPress = input('Choose a direction - ahead, back, left right: ')
+    return keyPress[0].lower()
 
 
 player_instance = player()
