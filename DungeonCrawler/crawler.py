a9c690c8bf2116b8c8857e15fa5bf145ccb27da5
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-28 15:40:07 -0600

    big text finished
    finished the big text for the intro. Might add the do do do DOOO a different way... unsure

--- DungeonCrawler/crawler.py
+++ DungeonCrawler/crawler.py
@@ -130,7 +130,7 @@ def bigText(st):
     if st == "dungeon crawler":
         x = 75
         y = 5
-        for i in range(0, 11):
+        for i in range(0, 12):
             print('')
             for j in range(0, 76):
                 if i == 0 and ((0 < j < 6) or (j == 10 or j == 17) or (j == 21 or j == 28) or (35 < j < 41) or (45 < j < 53) or (58 < j < 62) or (j == 67 or j == 75)):  # first line
@@ -143,22 +143,22 @@ def bigText(st):
                     print('@', end='')
                 elif i == 4 and ((0 < j < 6) or (11 < j < 16) or (j == 21 or j == 28 or j == 27) or (34 < j < 40) or (45 < j < 53) or (58 < j < 62) or (j == 67 or j == 74 or j == 75)):  # D line 5
                     print('@', end='')
-                elif i == 5 and ((3 < j < 7) or (9 < j < 16) or (22 < j < 25)):
+                elif i == 5 and j == 0:
+                    print('\n', end ='')
+                elif i == 5 and ((4 < j < 8) or (10 < j < 17) or (23 < j < 26) or (j == 31 or j == 41) or (j == 45) or (56 < j < 63) or (65 < j < 72)):
                     print('@', end='')
-                elif i == 6 and ((j == 2 or j == 7) or (j == 10 or j == 16) or (j == 22 or j == 25)):
+                elif i == 6 and ((j == 2 or j == 7) or (j == 10 or j == 17) or (j == 22 or j == 25) or (j == 30 or j == 40) or (j == 44) or (j == 56) or (j == 65 or j == 72)):
                     print('@', end='')
-                elif i == 7 and ((j == 1) or (j == 10 or j == 17) or (j == 21 or j == 26)):
+                elif i == 7 and ((j == 1) or (j == 10 or j == 17) or (j == 21 or j == 26) or (j == 30 or j == 40 or j == 35) or (j == 44) or (55 < j < 61) or (j == 65 or j == 72)):
                     print('@', end='')
-                elif i == 8 and ((j == 1) or (j == 10 or j == 16 or j == 15 or j == 14) or (20 < j < 27)):
+                elif i == 8 and ((j == 2 or j == 7) or (j == 10 or 14 < j < 17) or (19 < j < 28) or (j == 30 or j == 40 or j == 34 or j == 36) or (j == 44) or (j == 56) or (j == 65 or 69 < j < 72)):
                     print('@', end='')
-                elif i == 9 and ((j == 2 or j == 7) or (j == 10 or j == 15) or (j == 20 or j == 27)):
-                    print('@', end='')
-                elif i == 10 and ((3 < j < 7) or (j == 10 or j == 16) or (j == 19 or j == 28)):
+                elif i == 9 and ((3 < j < 7) or (j == 10 or j == 17) or (j == 19 or j == 28) or (j == 31 or j == 39 or j == 33 or j == 37) or (43 < j < 52) or (55 < j < 62) or (j == 65 or j == 72)):
                     print('@', end='')
                 else:
                     print(' ', end='')
 
-        print(st + '\n')
+        print('\n')
     elif st == 'do do do DOOOO':
         print(st + '\n')
     else:
