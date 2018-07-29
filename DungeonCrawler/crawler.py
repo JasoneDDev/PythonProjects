6817ce05686b2338d0fa8ed5a722d77bc900fcac
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-28 21:26:56 -0600

    breaking out intro text def
    intro text def is large chunk of code and best to break it out to keep the main code cleaner

--- DungeonCrawler/crawler.py
+++ DungeonCrawler/crawler.py
@@ -2,6 +2,7 @@ import numpy as np
 import time as t
 import random as r
 from playerClass import player
+from dcIntro import bigText
 
 # things to add: except statements, yields
 # High score.. save out to file and read in from file (high score is time based)
@@ -129,46 +130,6 @@ def randomKey():
 itemLocations = [[0, 4], randomKey()]
 itemDefs = [['exit', 1], ['key', 3]]
 directionList = ['South', 'West', 'North', 'East']
-
-
-
-def bigText(st):
-    if st == "dungeon crawler":
-        x = 75
-        y = 5
-        for i in range(0, 12):
-            print('')
-            for j in range(0, 76):
-                if i == 0 and ((0 < j < 6) or (j == 10 or j == 17) or (j == 21 or j == 28) or (35 < j < 41) or (45 < j < 53) or (58 < j < 62) or (j == 67 or j == 75)):  # first line
-                    print('@', end='')
-                elif i == 1 and ((j == 1 or j == 7) or (j == 10 or j ==17) or (j == 21 or j == 28 or j == 23) or (j == 34) or (j == 46) or (j == 57 or j == 63) or (j == 67 or j == 69 or j == 75)):  # line 2
-                    print('@', end='')
-                elif i == 2 and ((j == 1 or j == 7) or (j == 10 or j ==17) or (j == 21 or j == 28 or j == 24) or (j == 33 or 38 < j < 42) or (45 < j < 52) or (j == 56 or j == 64) or (j == 67 or j == 70 or j == 75)):  # line 3
-                    print('@', end='')
-                elif i == 3 and ((j == 1 or j == 7) or (j == 10 or j ==17) or (j == 21 or j == 28 or j == 25) or (j == 33 or j == 41) or (j == 46) or (j == 57 or j == 63) or (j == 67 or j == 72 or j == 75)):  # D line 4
-                    print('@', end='')
-                elif i == 4 and ((0 < j < 6) or (11 < j < 16) or (j == 21 or j == 28 or j == 27) or (34 < j < 40) or (45 < j < 53) or (58 < j < 62) or (j == 67 or j == 74 or j == 75)):  # D line 5
-                    print('@', end='')
-                elif i == 5 and j == 0:
-                    print('\n', end ='')
-                elif i == 5 and ((4 < j < 8) or (10 < j < 17) or (23 < j < 26) or (j == 31 or j == 41) or (j == 45) or (56 < j < 63) or (65 < j < 72)):
-                    print('@', end='')
-                elif i == 6 and ((j == 2 or j == 7) or (j == 10 or j == 17) or (j == 22 or j == 25) or (j == 30 or j == 40) or (j == 44) or (j == 56) or (j == 65 or j == 72)):
-                    print('@', end='')
-                elif i == 7 and ((j == 1) or (j == 10 or j == 17) or (j == 21 or j == 26) or (j == 30 or j == 40 or j == 35) or (j == 44) or (55 < j < 61) or (j == 65 or j == 72)):
-                    print('@', end='')
-                elif i == 8 and ((j == 2 or j == 7) or (j == 10 or 14 < j < 17) or (19 < j < 28) or (j == 30 or j == 40 or j == 34 or j == 36) or (j == 44) or (j == 56) or (j == 65 or 69 < j < 72)):
-                    print('@', end='')
-                elif i == 9 and ((3 < j < 7) or (j == 10 or j == 17) or (j == 19 or j == 28) or (j == 31 or j == 39 or j == 33 or j == 37) or (43 < j < 52) or (55 < j < 62) or (j == 65 or j == 72)):
-                    print('@', end='')
-                else:
-                    print(' ', end='')
-
-        print('\n')
-    elif st == 'do do do DOOOO':
-        print(st + '\n')
-    else:
-        pass
 
 
 
