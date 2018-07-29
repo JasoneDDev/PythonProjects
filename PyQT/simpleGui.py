4f4ee9d5b0a1a3c7bb986de03d6f9d7541df224f
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-07-29 15:08:22 -0600

    pyQTv5 sample
    just making a simple pyQT example with some labels, buttons, graphics etc. pretty simple overrall

--- PyQT/simpleGui.py
+++ PyQT/simpleGui.py
@@ -1,12 +1,22 @@
 import sys
-from PyQt4 import QtGui
+from PyQt5 import QtWidgets, QtGui
 
-app = QtGui.QApplication(sys.argv)
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    l1 = QtWidgets.QLabel(w)
+    l2 = QtWidgets.QLabel(w)
+    l1.setText('Green Clown Goby')
+    l2.setPixmap(QtGui.QPixmap('goby.png'))
+    l1.move(100, 25)
+    l2.move(100, 50)
+    w.setWindowTitle('Sample GUI demo')
+    w.setGeometry(200, 100,800, 500)
+    w.show()
+    sys.exit(app.exec_())
 
-window = QtGui.QWidget()
-window.setGeometry(50, 50, 500, 300)
-window.setWindowTitle("Dungeon Crawler")
 
-window.show()
+window()
+
 
 
