77e548b6dc03fe6d282280d5e0281566370d3789
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-08-07 14:24:19 -0600

    score keeper and vendasta question

--- TextJeopardy/test_scoreKeeper.py
+++ TextJeopardy/test_scoreKeeper.py
@@ -1,24 +1,54 @@
 import unittest
 import scoreKeeper as sk
+from unittest.mock import patch # this is for data out of your control ie websites etc. able to mock data and get responses etc.
 
 class TestScore(unittest.TestCase):
 
+    @classmethod
+    def SetUpClass(cls):
+        # initial setup (databases etc)
+        pass
+
+    @classmethod
+    def TearDownClass(cls):
+        # final tear down
+        pass
+
+    # setup / teardown is before and after each test
+    def setUp(self):
+        self.scorek = sk.ScoreKeeper()
+    def tearDown(self):
+        # example of things to add would be to remove files if testing if files were created by a function.
+        pass
+
     def test_add2Score(self):
-        scorek = sk.ScoreKeeper()
-        scorek.add2score(5)
-        self.assertEquals(scorek.add2score(10), 15)
-        self.assertEquals(scorek.add2score(35), 50)
-        self.assertEquals(scorek.add2score(120), 170)
+        self.scorek.add2score(5)
+        self.assertEquals(self.scorek.add2score(10), 15)
+        self.assertEquals(self.scorek.add2score(35), 50)
+        self.assertEquals(self.scorek.add2score(120), 170)
 
     def test_subFromScore(self):
-        scorek2 = sk.ScoreKeeper()
-        scorek2.add2score(170)
-        self.assertEquals(scorek2.subFromScore(10), 160)
-        self.assertEquals(scorek2.subFromScore(35), 125)
-        self.assertEquals(scorek2.subFromScore(120), 5)
+        self.scorek.add2score(170)
+        self.assertEquals(self.scorek.subFromScore(10), 160)
+        self.assertEquals(self.scorek.subFromScore(35), 125)
+        self.assertEquals(self.scorek.subFromScore(120), 5)
 
         #with self.assertRaises(ValueError):
          #   scorek2.subFromScore('a')  # or something that will raise an exception of value error
+
+    def test_mockData(self):
+        pass
+        #with patch(employee.request.get) as mocked_get: # uses patch as context manager "with" ...this is swapping the get call with our patch/mock method
+        #    # will use mocked instead of the actual request
+        #    mocked_get.return_value.ok = True # here we set what should come back
+        #    mocked_get.return_value.text = 'Success' # set what should come back here as well
+
+        #    schedule = self.emp_1.monthly_Schedule('May') # setup as if we were testing it here and this calls the function
+        #    mocked_get.assert_called_with('http://companyName.com/shafer/May') # testing if got called with proper URL
+        #    self.assertEqual(schedule, 'Success') # testing if the return value came in correctly... ie 'sucess' set above
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ vendastaProblem.py
@@ -0,0 +1,61 @@
+#11, 3, 5
+
+#11 boxes max 3 and needs to be split 5 way
+
+boxes = 11
+max_height = 3
+
+numOfStacks =0
+
+def splitDivide(quantity, maxHeight):
+    if quantity > maxHeight:
+      a = quantity // 2
+      b = quantity % 2 + a
+      print(a,b)
+      return a, b
+
+if boxes > max_height:
+    a,b = splitDivide(boxes, max_height)
+    if a > max_height:
+        aa, bb = splitDivide(a, max_height)
+        if aa > max_height:
+            aaa, bbb = splitDivide(aa, max_height)
+            if aaa > max_height:
+                pass
+            else:
+                numOfStacks+=1
+            if bbb > max_height:
+                pass
+            else:
+                numOfStacks+=1
+        else:
+            numOfStacks+=1
+        if bb > max_height:
+            aaa, bbb = splitDivide(bb, max_height)
+            if aaa > max_height:
+                pass
+            else:
+                numOfStacks+=1
+            if bbb > max_height:
+                pass
+            else:
+                numOfStacks+=1
+        else:
+            numOfStacks+=1
+    else:
+        numOfStacks+=1
+    if b > max_height:
+        cc, dd = splitDivide(b, max_height)
+        if cc > max_height:
+            pass
+        else:
+            numOfStacks+=1
+        if dd > max_height:
+            pass
+        else:
+            numOfStacks+=1
+    else:
+        numOfStacks+=1
+
+
+print(numOfStacks)
