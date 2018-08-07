143c4f3d9858fa44ad9067dfd0db0dfdc9301db4
JasoneDDev <41653034+JasoneDDev@users.noreply.github.com>
2018-08-07 14:48:03 -0600

    vendasta

--- vendastaProblem.py
+++ vendastaProblem.py
@@ -5,75 +5,26 @@
 boxes = 13
 max_height = 3
 
-numOfStacks =0
+numOfStacks = 0
+
 
 def splitDivide(quantity, maxHeight):
+    global numOfStacks
     if quantity > maxHeight:
       a = quantity // 2
       b = quantity % 2 + a
+      if a <= maxHeight:
+          numOfStacks += 1
+      else:
+          splitDivide(a, max_height)
+      if b <= maxHeight:
+          numOfStacks += 1
+      else:
+          splitDivide(b, max_height)
       print(a,b)
       return a, b
+    return numOfStacks
 
 
-
-if boxes > max_height:
-    a,b = splitDivide(boxes, max_height)
-    if a > max_height:
-        aa, bb = splitDivide(a, max_height)
-        if aa > max_height:
-            aaa, bbb = splitDivide(aa, max_height)
-            if aaa > max_height:
-                pass
-            else:
-                numOfStacks+=1
-            if bbb > max_height:
-                pass
-            else:
-                numOfStacks+=1
-        else:
-            numOfStacks+=1
-        if bb > max_height:
-            aaa, bbb = splitDivide(bb, max_height)
-            if aaa > max_height:
-                pass
-            else:
-                numOfStacks+=1
-            if bbb > max_height:
-                pass
-            else:
-                numOfStacks+=1
-        else:
-            numOfStacks+=1
-    else:
-        numOfStacks+=1
-    if b > max_height:
-        cc, dd = splitDivide(b, max_height)
-        if cc > max_height:
-            ccc, ddd = splitDivide(cc, max_height)
-            if ccc > max_height:
-                pass
-            else:
-                numOfStacks+=1
-            if ddd > max_height:
-                pass
-            else:
-                numOfStacks+=1
-        else:
-            numOfStacks+=1
-        if dd > max_height:
-            ccc, ddd = splitDivide(dd, max_height)
-            if ccc > max_height:
-                pass
-            else:
-                numOfStacks += 1
-            if ddd > max_height:
-                pass
-            else:
-                numOfStacks += 1
-        else:
-            numOfStacks+=1
-    else:
-        numOfStacks+=1
-
-
+splitDivide(boxes, max_height)
 print(numOfStacks)
